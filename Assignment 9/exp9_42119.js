56582d618e02eea4238726d9b49801b56913a874
Param <66090650+ParamChordiya@users.noreply.github.com>
2022-12-09 10:49:09 +0530

    Add files via upload

---
+++ Assignment 9/exp9_42119.js
@@ -0,0 +1,12 @@
+// Name: Param Chordiya
+// Roll Number: 42119
+// Batch: P5 - BE5
+// Subject: Javascript
+var table = 9;
+var length = 10;
+var i = 1;
+document.write("Multiplication Table :" + table);
+while (i <= length) {
+    document.write("<br>" + i + "*" + table + "=" + (i * table));
+    i++;
+}
